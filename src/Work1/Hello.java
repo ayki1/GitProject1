921c5f6bf0fb5f3e74eb0d03836a3428c50aff08
AyhanK <ayhankaykac@gmail.com>
2022-04-07 22:15:52 +0300

    Added more Hello

--- src/Work1/Hello.java
+++ src/Work1/Hello.java
@@ -3,9 +3,10 @@ package Work1;
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Work1 started");
+        System.out.println("Hello, Work1 started");
         System.out.println("Created Git Repository");
         System.out.println("Made few commit");
+        System.out.println("Local repo id already published to Github");
 
 
     }
